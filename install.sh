c2670a98e6717127898a05dd1b950538a80e52bc
Dmitry Grach <dmitrygrach@gmail.com>
2015-05-12 11:43:44 +0300

    base instalation with brew

---
+++ install.sh
@@ -0,0 +1,21 @@
+# Mac OS X auto configuration
+# Dmitry Grach, A.cademy
+# MIT License
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew install caskroom/cask/brew-cask
+brew cask install virtualbox
+brew install boot2docker
+brew install docker
+brew install nmap
+brew install unrar
+brew install tig
+brew cask install firefox --HEAD
+brew cask install transmission
+brew install tmux
+brew cask install lighttable
+brew install rbenv
+brew install ruby-build
+brew cask install anvil
+brew install ffind
