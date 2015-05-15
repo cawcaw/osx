1a13ce319092108c90ba753e78f9b48e7f02d501
Dmitry Grach <dmitrygrach@gmail.com>
2015-05-15 18:30:36 +0300

    list each installation

--- install.sh
+++ install.sh
@@ -4,24 +4,28 @@
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
+formulas= (boot2docker
+           docker
+           nmap
+           unrar
+           tig
+           tmux
+           rbenv
+           ruby-build
+           ffind
+           git
+           ffind
+           tree
+           z)
+brew install ${formulas[@]}
+
 brew install caskroom/cask/brew-cask
-brew cask install virtualbox
-brew install boot2docker
-brew install docker
-brew install nmap
-brew install unrar
-brew install tig
-brew cask install firefox --HEAD
-brew cask install transmission
-brew install tmux
-brew cask install lighttable
-brew install rbenv
-brew install ruby-build
-brew cask install anvil
-brew install ffind
-brew install git
-brew install ffind
-brew install z
+casks= (anvil
+        lighttable
+        transmission
+        firefox\ --HEAD
+        virtualbox)
+brew cask install ${casks[@]}
 
 cd ~
 git clone git://github.com/cawcaw/dotfiles
