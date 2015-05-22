bfcbbf45eb1234a8987abfd8e069e2bed7f06c12
Dmitry Grach <grach@MacBook-Pro-Dmitry.local>
2015-05-23 01:26:48 +0300

    fix arrays of pkgs and chmod +x

--- install.sh
+++ install.sh
@@ -4,27 +4,27 @@
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
-formulas= (boot2docker
-           docker
-           nmap
-           unrar
-           tig
-           tmux
-           rbenv
-           ruby-build
-           ffind
-           git
-           ffind
-           tree
-           z)
+formulas=(boot2docker
+          docker
+          nmap
+          unrar
+          tig
+          tmux
+          rbenv
+          ruby-build
+          ffind
+          git
+          ffind
+          tree
+          z)
 brew install ${formulas[@]}
 
 brew install caskroom/cask/brew-cask
-casks= (anvil
-        lighttable
-        transmission
-        firefox\ --HEAD
-        virtualbox)
+casks=(anvil
+       lighttable
+       transmission
+       firefox\ --HEAD
+       virtualbox)
 brew cask install ${casks[@]}
 
 cd ~
